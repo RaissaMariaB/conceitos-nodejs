a852e4894fb4b1bd87b4a9aecc70e3c7c932a13d
RaissaBarata <raissamaria.sb@gmail.com>
2020-07-08 23:35:36 -0300

    Add logic of update and delete  project

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-   const { title, url, techs } = request.body
+  const { title, url, techs } = request.body
 
   const repository = {
     id: uuid(), 
@@ -37,35 +37,50 @@ app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const { title, url, techs } = request.body;
 
-  const projectIncludes = repositories.includes( idProject => idProject.id === id);
+  const project = repositories.find(project => project.id === id);
 
-  if( !projectIncludes ){
-    response.status(400).json({error:"This project dosen't exist."})
+  if (!project) {
+    response.status(400).json({error:"This project doesn't exist."})
   };
 
-  const newProject = {
-    title,
-    url,
-    techs,
+  const updatedProject = {
+    id: project.id,
+    title: title ? title : project.title,
+    url: url ? url : project.url,
+    techs: techs ? techs : project.techs,
+    likes: project.likes,
   };
 
-  repositories[projectIncludes] = newProject;
+  const indexProject = repositories.indexOf(project)
 
-  return response.json(newProject);
+  repositories.splice(project, 1, indexProject);
 
+  console.log(updatedProject);
+  return response.json(updatedProject);
 });
 
 app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;  
+
+  const indexRepository = repositories.findIndex(repository => repository.id == id)
+
+  if( indexRepository === -1 ){
+    return response.status(400).json();
+  }
+  
+  repositories.splice(indexRepository, 1);
+
+  return response.status(204).json(repositories)
   
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  const { id } = request.params;
+  const { id } = request.params;  
 
-  const repository = repositories.find(repository => repository.id === id)
+  const repository = repositories.find(repository => repository.id == id)
 
   if( !repository ){
-    return response.json(400)
+    return response.status(400).json();
   }
 
   repository.likes += 1 ;
